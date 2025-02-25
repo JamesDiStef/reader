be21e7611d99b1622b6c00b968efd73efa764868
James <james.distef@gmail.com>
2025-02-24 19:01:43 -0500

    make user page server component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,14 +1,12 @@
-"use client";
-
-import { useEffect } from "react";
 import BookFound from "../components/BookFound";
 import { useUser } from "../userContext";
+import { redirect } from "next/navigation";
 
 const Page = () => {
-  const { bookList } = useUser();
+  const { user, bookList } = useUser();
   let count = 0;
 
-  useEffect(() => {}, []);
+  if (!user) redirect("/");
 
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 w-full">
