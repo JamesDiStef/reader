4a74b249138eb49dd9317240550b84ad60558eac
James <james.distef@gmail.com>
2025-02-27 18:39:24 -0500

    create user on new login

--- app/login.tsx
+++ app/login.tsx
@@ -14,7 +14,22 @@ const HomeRedirect = () => {
     setUser(inputValue);
     router.push("/search");
     const response = await fetch(`/api/users/${inputValue}`);
-    const user = await response.json();
+    let user = await response.json();
+    console.log(user.error);
+    if (user.error) {
+      const newUser = {
+        userId: inputValue,
+        bookList: [],
+      };
+      await fetch(`https://reader-teal-pi.vercel.app/api/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      });
+      user = newUser;
+    }
     const bookIds = user.bookList;
     const books: Book[] = [];
     for (let i = 0; i < bookIds?.length; i++) {
@@ -28,7 +43,7 @@ const HomeRedirect = () => {
     <div className="flex flex-col mt-[25%] sm:mt-[15%] mx-auto w-1/2 h-full justify-center items-center text-black ">
       <div className="text-xl m-3 bg-slate-400 opacity-85 p-5 rounded-md">
         <div className="text-2xl text-center">Login</div>
-        If you have a unique user id, enter it below!
+        Enter your id below
       </div>
       <input
         className="flex w-1/2 min-h-[40px] text-center m-3"
--- app/search/SearchBar.tsx
+++ app/search/SearchBar.tsx
@@ -9,7 +9,6 @@ const SearchBar = () => {
   const { replace } = useRouter();
 
   const handleTyping = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
     const params = new URLSearchParams(searchParams);
     if (e.target.value) {
       params.set("query", e.target.value);
@@ -26,11 +25,6 @@ const SearchBar = () => {
         defaultValue={searchParams.get("query")?.toString()}
         onChange={(e) => handleTyping(e)}
       ></input>
-      <div className="flex justify-center">
-        <button className="bg-blue-600 text-white rounded-lg mt-2 mx-auto h-12 px-6 w-48 transition-all duration-300 transform hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-opacity-50 active:scale-95">
-          Search!
-        </button>
-      </div>
     </div>
   );
 };
