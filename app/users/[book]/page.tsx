8071bc15e2ca3a5bdf779e91408464dc88c4c9bc
James <james.distef@gmail.com>
2025-09-16 19:47:57 -0400

    new page

---
+++ app/users/[book]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className="flex flex-col h-full items-center justify-center">
+        <h1 className="">Book name</h1>
+        <h2>Author</h2>
+        <p>review</p>
+    </div>
+  )
+}
+
+export default Page;
